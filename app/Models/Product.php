eb196084bf357d2bc95279807071b9d4bf5def6f
Anthony Rosado <arosado-26@hotmail.com>
2024-03-23 16:42:08 -0500

    Add product model

---
+++ app/Models/Product.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
+
+/**
+ * App\Models\Product
+ *
+ * @property int $id
+ * @property string $code
+ * @property string $name
+ * @property string $description
+ * @property bool $is_available
+ * @property string $price
+ * @property int $stock
+ * @property string|null $image_url
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static Builder|Product newModelQuery()
+ * @method static Builder|Product newQuery()
+ * @method static Builder|Product query()
+ * @method static Builder|Product whereCode($value)
+ * @method static Builder|Product whereCreatedAt($value)
+ * @method static Builder|Product whereDescription($value)
+ * @method static Builder|Product whereId($value)
+ * @method static Builder|Product whereImageUrl($value)
+ * @method static Builder|Product whereIsAvailable($value)
+ * @method static Builder|Product whereName($value)
+ * @method static Builder|Product wherePrice($value)
+ * @method static Builder|Product whereStock($value)
+ * @method static Builder|Product whereUpdatedAt($value)
+ */
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'code',
+        'name',
+        'description',
+        'is_available',
+        'price',
+        'stock',
+        'image_url',
+    ];
+
+    protected $casts = [
+        'is_available' => 'boolean',
+        'price' => 'decimal:2',
+    ];
+
+    protected $attributes = [
+        'is_available' => false,
+        'price' => 0,
+        'stock' => 0,
+    ];
+}
