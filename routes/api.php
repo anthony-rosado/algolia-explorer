d24d6902c50fdfa21650c8598d0cc84287a40e71
Anthony Rosado <arosado-26@hotmail.com>
2024-03-23 17:20:20 -0500

    Add endpoint to get products

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\SignupController;
+use App\Http\Controllers\Products\GetProductsController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/auth/signup', SignupController::class);
 Route::post('/auth/login', LoginController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/products', GetProductsController::class);
+});
