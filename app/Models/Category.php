2a8675a052d907bf399732aa9a1fc08a1a766391
Anthony Rosado <arosado-26@hotmail.com>
2024-03-23 23:12:56 -0500

    Update category model docblock

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Database\Factories\CategoryFactory;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -19,8 +20,12 @@ use Illuminate\Support\Carbon;
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
  * @property int|null $parent_id
- * @method static CategoryFactory factory($count = null, $state = [])
+ * @property-read Collection<int, Category> $children
+ * @property-read int|null $children_count
  * @property-read Category|null $parent
+ * @property-read Collection<int, Product> $products
+ * @property-read int|null $products_count
+ * @method static CategoryFactory factory($count = null, $state = [])
  * @method static Builder|Category newModelQuery()
  * @method static Builder|Category newQuery()
  * @method static Builder|Category query()
