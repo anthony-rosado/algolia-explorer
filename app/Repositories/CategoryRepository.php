a1ed15eccc5e32cf6520378b6239723e49618747
Anthony Rosado <arosado-26@hotmail.com>
2024-03-31 18:30:24 -0500

    Implement repository method to find category by id

--- app/Repositories/CategoryRepository.php
+++ app/Repositories/CategoryRepository.php
@@ -2,7 +2,14 @@
 
 namespace App\Repositories;
 
+use App\Models\Category;
+
 class CategoryRepository
 {
-
+    public function findById(int $id): ?Category
+    {
+        return Category::query()
+            ->where('id', '=', $id)
+            ->first();
+    }
 }
