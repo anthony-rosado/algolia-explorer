5cdf659885d4867d04061f75ea30c5d705bf9c73
Anthony Rosado <arosado-26@hotmail.com>
2024-03-31 18:33:14 -0500

    Implement repository method to find product by id

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -2,7 +2,14 @@
 
 namespace App\Repositories;
 
+use App\Models\Product;
+
 class ProductRepository
 {
-
+    public function findByCode(string $code): ?Product
+    {
+        return Product::query()
+            ->where('code', '=', $code)
+            ->first();
+    }
 }
