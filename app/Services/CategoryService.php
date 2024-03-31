c4e6a258420f9dda4201273e367d03a760fd1fd0
Anthony Rosado <arosado-26@hotmail.com>
2024-03-31 18:31:19 -0500

    Implement service method to find category by id

--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -2,7 +2,17 @@
 
 namespace App\Services;
 
-class CategoryService
-{
+use App\Models\Category;
+use App\Repositories\CategoryRepository;
 
+readonly class CategoryService
+{
+    public function __construct(private CategoryRepository $repository)
+    {
+    }
+
+    public function findById(int $id): ?Category
+    {
+        return $this->repository->findById($id);
+    }
 }
