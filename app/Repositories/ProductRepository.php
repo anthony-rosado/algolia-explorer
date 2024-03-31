c24f4068e4cf58b7f8f56c2d664d11f227b71cae
Anthony Rosado <arosado-26@hotmail.com>
2024-03-31 18:34:20 -0500

    Implement repository method to create product

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -12,4 +12,28 @@ class ProductRepository
             ->where('code', '=', $code)
             ->first();
     }
+
+    public function create(
+        string $code,
+        string $name,
+        string $description,
+        bool $isAvailable,
+        float $price,
+        int $stock,
+        ?string $imageUrl,
+        int $categoryId,
+    ): Product {
+        $product = new Product();
+        $product->code = $code;
+        $product->name = $name;
+        $product->description = $description;
+        $product->is_available = $isAvailable;
+        $product->price = $price;
+        $product->stock = $stock;
+        $product->image_url = $imageUrl;
+        $product->category()->associate($categoryId);
+        $product->save();
+
+        return $product;
+    }
 }
