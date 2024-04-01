ae0243a23df303758216857070b89d12aa3c5ee3
Anthony Rosado <arosado-26@hotmail.com>
2024-03-31 23:47:10 -0500

    Refactor handler to delete product

--- app/Http/Controllers/Products/DeleteProductController.php
+++ app/Http/Controllers/Products/DeleteProductController.php
@@ -2,15 +2,50 @@
 
 namespace App\Http\Controllers\Products;
 
+use App\Exceptions\Products\ProductNotFoundByIdException;
 use App\Http\Controllers\Controller;
-use App\Models\Product;
+use App\Services\ProductService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\Response as FoundationResponse;
+use Throwable;
 
 class DeleteProductController extends Controller
 {
-    public function __invoke(Product $product): Response
+    public function __construct(private readonly ProductService $service)
     {
-        $product->delete();
+    }
+
+    public function __invoke(int $productId): Response|JsonResponse
+    {
+        try {
+            $product = $this->service->findById($productId);
+
+            if ($product === null) {
+                throw new ProductNotFoundByIdException($productId);
+            }
+
+            $this->service->setModel($product);
+            $this->service->delete();
+        } catch (ProductNotFoundByIdException $exception) {
+            return response()->json(
+                [
+                    'error' => [
+                        'message' => $exception->getMessage(),
+                    ],
+                ],
+                FoundationResponse::HTTP_NOT_FOUND,
+            );
+        } catch (Throwable $exception) {
+            return response()->json(
+                [
+                    'error' => [
+                        'message' => $exception->getMessage(),
+                    ],
+                ],
+                FoundationResponse::HTTP_INTERNAL_SERVER_ERROR,
+            );
+        }
 
         return response()->noContent();
     }
