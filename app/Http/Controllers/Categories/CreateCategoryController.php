c1867a13a9ee2a067831ba3e23f717bad4fdd041
Anthony Rosado <arosado-26@hotmail.com>
2024-04-01 22:07:18 -0500

    Refactor handler to create category

--- app/Http/Controllers/Categories/CreateCategoryController.php
+++ app/Http/Controllers/Categories/CreateCategoryController.php
@@ -2,48 +2,51 @@
 
 namespace App\Http\Controllers\Categories;
 
+use App\Exceptions\Categories\CategoryNotFoundByIdException;
+use App\Exceptions\Categories\InvalidParentAssignmentException;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Categories\CreateCategoryRequest;
 use App\Http\Resources\Categories\CategoryResource;
-use App\Models\Category;
+use App\Services\CategoryService;
 use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class CreateCategoryController extends Controller
 {
+    public function __construct(private readonly CategoryService $service)
+    {
+    }
+
     public function __invoke(CreateCategoryRequest $request): CategoryResource|JsonResponse
     {
-        $parentCategory = null;
+        try {
+            $this->service->create(
+                $request->input('name'),
+                $request->input('description'),
+                $request->input('parent_id')
+            );
 
-        if ($request->filled('parent_id')) {
-            $parentCategory = Category::query()->find($request->input('parent_id'));
+            $category = $this->service->getModel();
 
-            if (is_null($parentCategory)) {
-                return response()->json(
-                    [
-                        'error' => [
-                            'message' => 'Parent category not found',
-                        ]
+            return CategoryResource::make($category);
+        } catch (CategoryNotFoundByIdException $exception) {
+            return response()->json(
+                [
+                    'error' => [
+                        'message' => $exception->getMessage(),
                     ],
-                    404
-                );
-            }
-
-            if (!$parentCategory->isParent()) {
-                return response()->json(
-                    [
-                        'error' => [
-                            'message' => "Category '{$parentCategory->name}' is not a parent category",
-                        ]
+                ],
+                Response::HTTP_NOT_FOUND
+            );
+        } catch (InvalidParentAssignmentException $exception) {
+            return response()->json(
+                [
+                    'error' => [
+                        'message' => $exception->getMessage(),
                     ],
-                    400
-                );
-            }
+                ],
+                Response::HTTP_BAD_REQUEST
+            );
         }
-
-        $category = Category::query()->make($request->only(['name', 'description']));
-        $category->parent()->associate($parentCategory);
-        $category->save();
-
-        return CategoryResource::make($category);
     }
 }
